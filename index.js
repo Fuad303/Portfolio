47f9a060a128ff7b25a53347c371d20771b24da3
Fuad303 <sadiqovfuad8@gmail.com>
2022-09-17 10:11:31 +0400

    some possibly meaningful message

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const navOpen = () =>
 {
     navList.classList.add('show');
     navBgOverlayEl.classList.add('active');
-    document.body.style='visibility: visible, height: 100vh; width: 100vw; overflow: auto;';
+    document.body.style='visibility: visible, height: 100vh; width: 100vw; overflow-x: hidden;';
 }
 const navClose = () =>
 {
